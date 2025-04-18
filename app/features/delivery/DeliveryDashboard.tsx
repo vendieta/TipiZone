d6917caeeaaf2a9e6e1ebdd78a92495e5110dc8a
vendieta <vendieta5@gmail.com>
2025-04-17 20:07:45 -0500

    small change

--- app/features/delivery/DeliveryDashboard.tsx
+++ app/features/delivery/DeliveryDashboard.tsx
@@ -1,142 +1,15 @@
-// import {
-//   View,
-//   Text,
-//   StyleSheet,
-//   SafeAreaView,
-//   FlatList,
-//   RefreshControl,
-//   ActivityIndicator,
-// } from 'react-native';
-// import React, {useEffect, useState} from 'react';
-// import {Colors} from '@/src/utils/Constants';
-// import {useAuthStore} from '@/src/state/authStore';
-// import DeliveryHeader from '@/components/delivery/DeliveryHeader';
-// import TabBar from '@/components/delivery/TabBar';
-// // import Geolocation from '@react-native-community/geolocation';
-// // import {reverseGeocode} from '@/src/service/mapService';
-// import {fetchOrders} from '@/src/service/orderService';
-// import DeliveryOrderItem from '@/components/delivery/DeliveryOrderItem';
-// import CustomText from '@/components/ui/CustomText';
-// import withLiveOrder from './withLiveOrder';
-
-// const DeliveryDashboard = () => {
-//   const {user, setUser} = useAuthStore();
-//   const [selectedTab, setSelectedTab] = useState<'available' | 'delivered'>(
-//     'available',
-//   );
-//   const [loading, setLoading] = useState(true);
-//   const [data, setData] = useState<any[]>([]);
-//   const [refreshing, setRefreshing] = useState(false);
-
-//   // const updateUser = () => {
-//   //   Geolocation.getCurrentPosition(
-//   //     position => {
-//   //       const {latitude, longitude} = position.coords;
-//   //       reverseGeocode(latitude, longitude, setUser);
-//   //     },
-//   //     err => console.log(err),
-//   //     {
-//   //       enableHighAccuracy: false,
-//   //       timeout: 15000,
-//   //     },
-//   //   );
-//   // };
-
-//   // useEffect(() => {
-//   //   updateUser();
-//   // }, []);
-
-//   const fetchData = async () => {
-//     setData([]);
-//     setRefreshing(true);
-//     setLoading(true);
-//     const data = await fetchOrders(selectedTab, user?._id, user?.branch);
-//     setData(data);
-//     setRefreshing(false);
-//     setLoading(false);
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, [selectedTab]);
-
-//   const renderOrderItem = ({item, index}: any) => {
-//     return <DeliveryOrderItem index={index} item={item} />;
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <SafeAreaView>
-//         <DeliveryHeader name={user?.name} email={user?.email} />
-//       </SafeAreaView>
-//       <View style={styles.subContainer}>
-//         <TabBar selectedTab={selectedTab} onTabChange={setSelectedTab} />
-
-//         <FlatList
-//           data={data}
-//           refreshControl={
-//             <RefreshControl
-//               refreshing={refreshing}
-//               onRefresh={async () => await fetchData()}
-//             />
-//           }
-//           ListEmptyComponent={() => {
-//             if (loading) {
-//               return (
-//                 <View style={styles.center}>
-//                   <ActivityIndicator color={Colors.secondary} size="small" />
-//                 </View>
-//               );
-//             }
-//             return (
-//               <View style={styles.center}>
-//                 <CustomText>No Orders found yet!</CustomText>
-//               </View>
-//             );
-//           }}
-//           renderItem={renderOrderItem}
-//           keyExtractor={item => item.orderId}
-//           contentContainerStyle={styles.flatlistContaienr}
-//         />
-//       </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: Colors.primary,
-//     flex: 1,
-//     paddingTop: 20
-//   },
-//   subContainer: {
-//     backgroundColor: Colors.backgroundSecondary,
-//     flex: 1,
-//     padding: 6,
-//   },
-//   flatlistContaienr: {
-//     padding: 2,
-//   },
-//   center: {
-//     flex: 1,
-//     marginTop: 60,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
-
-// export default withLiveOrder(DeliveryDashboard)
-
-
-import { View, StyleSheet} from "react-native";
+import { View, StyleSheet, ScrollView, Image } from "react-native";
 
 
 export default function DeliveryDashboard (){
     
     return(
         <View style={styles.container}>
-            
-        
+            <ScrollView>
+                <View style={styles.header}>
+                    <Image style={styles.fondo} source={require('@assets/images/fondo2.png')}/>
+                </View>
+            </ScrollView>
         </View>
     );
 }
@@ -145,6 +18,14 @@ export default function DeliveryDashboard (){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#141252',
+    },
+    fondo: {
+
+    },
+    header: {
+        width: '100%',
+        height: 70
     }
 })
 
---
+++ components/delivery/state.tsx
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from "react-native";
+
+
+
+export default function state() {
+
+    return(
+        <View style={styles.container}>
+            <Text>Estado</Text>
+            <Text>Inactivo</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
